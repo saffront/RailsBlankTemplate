d5687dfababe2089e2e16f2c8ea9a599b5084fe3
saffront <jiasherntan@gmail.com>
2021-08-05 16:16:08 +0800

    added all functionality

---
+++ app/models/ability.rb
@@ -0,0 +1,27 @@
+class Ability
+  include CanCan::Ability
+
+  def initialize(user)
+    # Define abilities for the passed in user here. For example:
+
+    user ||= User.new # guest user (not logged in)
+    if user.superadmin_role?
+          can :manage, :all
+          can :access, :rails_admin       # only allow admin users to access Rails Admin
+          can :manage, :dashboard         # allow access to dashboard
+    end
+    if user.supervisor_role?
+          can :manage, User
+    end
+
+    # logged in users
+    if user.user_role?
+
+      can :manage, :all
+
+    end
+
+  end
+
+
+end
